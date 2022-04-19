f390ce2cfc8725566351d469c47a4d5fd7289cbc
BMK <bensonmkurian@gmail.com>
2022-04-20 00:11:50 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './App.css';
 import Auth from './components/Auth';
 import Layout from './components/Layout';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  // console.log(isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  // console.log(cartItems);
+
+  useEffect(() => {
+    fetch(
+      'https://redux-http-demo-default-rtdb.firebaseio.com/cartItems.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
+
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
